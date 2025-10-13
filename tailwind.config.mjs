3779452be080221f550c29046130bcdce0a5ceb2
rjkashyap <raj.kashyap@outlook.com>
2025-10-13 17:53:27 +1200

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,108 +1,47 @@
-/** @type {import('tailwindcss').Config} */
+import { defineConfig } from "tailwindcss/helpers";
 import defaultTheme from "tailwindcss/defaultTheme";
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: ["selector"],
-  safelist: [
-    {
-      pattern: /col-span-(\d+)/,
-      variants: ["lg"],
-    },
-    // Height
-    {
-      pattern: /h-(0|2|3|4|6|8|12|16|24|32)/,
-      variants: ["lg"],
-    },
-    // Text sizes for all screen sizes
-    {
-      pattern: /text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)/,
-      variants: ["lg"],
-    },
-    // Font weights
-    {
-      pattern:
-        /font-(thin|extralight|light|normal|medium|semibold|bold|extrabold|black)/,
-    },
-    // Alignments
-    {
-      pattern: /text-(left|center|right)/,
-    },
-  ],
+
+export default defineConfig({
+  darkMode: "class",
+  content: ["src/**/*.{astro,md,mdx,ts,tsx,js,vue,svelte}"],
   theme: {
     extend: {
       colors: {
-        transparent: "transparent",
         primary: {
-          50: "#FFFFFF",
-          100: "#FFFFFF",
-          200: "#FFFFFF",
-          300: "#FFFFFF",
-          400: "#FFFFFF",
-          500: "#000000",
-          600: "#000000",
-          700: "#000000",
-          800: "#000000",
-          900: "#000000",
-          950: "#000000",
+          50: "#f9f9f9",
+          100: "#f2f2f2",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+          950: "#0a0a0a",
+          DEFAULT: "#000000",
         },
         neutral: {
-          50: "#FFFFFF",
-          100: "#FFFFFF",
-          200: "#FFFFFF",
-          300: "#FFFFFF",
-          400: "#FFFFFF",
-          500: "#000000",
-          600: "#000000",
-          700: "#000000",
-          800: "#000000",
-          900: "#000000",
-          950: "#000000",
+          50: "#f9f9f9",
+          100: "#f2f2f2",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+          950: "#0a0a0a",
         },
       },
-      cursor: {
-        fancy: "url(https://www.svgrepo.com/show/269/color-picker.svg)",
-      },
-      fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
-        headings: ["Outfit Variable", ...defaultTheme.fontFamily.sans],
-      },
-      keyframes: {
-        dropdown: {
-          "0%": { transform: "translateY(-1rem)", opacity: 0 },
-          "100%": { transform: "translateY(0)", opacity: 1 },
-        },
-        fadeInShadowLight: {
-          "100%": {
-            boxShadow:
-              "0 20px 25px -5px rgba(15, 23, 42, .025), 0 8px 10px -6px rgba(15, 23, 42, .025);",
-          },
-        },
-        fadeInShadowDark: {
-          "100%": {
-            boxShadow:
-              "0 20px 25px -5px rgba(2, 6, 23, .25), 0 8px 10px -6px rgba(2, 6, 23, .25);",
-          },
-        },
-        fadeUp: {
-          "0%": { transform: "translateY(1rem)", opacity: 0 },
-          "100%": { transform: "translateY(0)", opacity: 1 },
-        },
-      },
-      animation: {
-        dropdown: "dropdown 300ms ease-in-out forwards",
-        fadeInShadowLight: "fadeInShadowLight 500ms ease-in-out forwards",
-        fadeInShadowDark: "fadeInShadowDark 500ms ease-in-out forwards",
-        fadeUp: "fadeUp 500ms ease-in-out forwards",
-      },
+      ringColor: ({ theme }) => ({
+        DEFAULT: theme("colors.neutral.400"),
+      }),
+      borderColor: ({ theme }) => ({
+        DEFAULT: theme("colors.neutral.300"),
+      }),
     },
   },
-  variants: {
-    animation: ["responsive"],
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("tailwindcss/plugin")(function ({ addVariant }) {
-      addVariant("dark-me", ".dark_&");
-    }),
-  ],
-};
+  plugins: [],
+});
