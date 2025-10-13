98a2c9e7e39287715608f42a73061be4ba9b7544
rjkashyap <raj.kashyap@outlook.com>
2025-10-13 21:14:31 +1200

    .

--- src/config/footerNavigation.ts
+++ src/config/footerNavigation.ts
@@ -2,110 +2,82 @@
 // ------------
 // Description: The footer navigation data for the website.
 export interface Logo {
-	src: string
-	alt: string
-	text: string
+  src: string
+  alt: string
+  text: string
 }
 
 export interface FooterAbout {
-	title: string
-	aboutText: string
-	logo: Logo
+  title: string
+  aboutText: string
+  logo: Logo
 }
 
 export interface SubCategory {
-	subCategory: string
-	subCategoryLink: string
+  subCategory: string
+  subCategoryLink: string
 }
 
 export interface FooterColumn {
-	category: string
-	subCategories: SubCategory[]
+  category: string
+  subCategories: SubCategory[]
 }
 
 export interface SubFooter {
-	copywriteText: string
+  copywriteText: string
 }
 
 export interface FooterData {
-	footerAbout: FooterAbout
-	footerColumns: FooterColumn[]
-	subFooter: SubFooter
+  footerAbout: FooterAbout
+  footerColumns: FooterColumn[]
+  subFooter: SubFooter
 }
 
 export const footerNavigationData: FooterData = {
-	footerAbout: {
-		title: 'Foxi.',
-		aboutText:
-			'Expertly made, responsive, accessible components in React and HTML ready to be used on your website or app. Just copy and paste them on your Tailwind CSS project.',
-		logo: {
-			src: '/logo.svg',
-			alt: 'The tailwind astro theme',
-			text: 'Foxi.'
-		}
-	},
-	footerColumns: [
-		{
-			category: 'Product',
-			subCategories: [
-				{
-					subCategory: 'Features',
-					subCategoryLink: '/features'
-				},
-				{
-					subCategory: 'FAQ',
-					subCategoryLink: '/faq'
-				},
-				{
-					subCategory: 'Pricing',
-					subCategoryLink: '/pricing'
-				},
-				{
-					subCategory: 'Changelog',
-					subCategoryLink: '/changelog'
-				},
-				{
-					subCategory: 'Terms',
-					subCategoryLink: '/terms'
-				}
-			]
-		},
-		{
-			category: 'About us',
-			subCategories: [
-				{
-					subCategory: 'About us',
-					subCategoryLink: '/'
-				},
-				{
-					subCategory: 'News',
-					subCategoryLink: '/blog'
-				},
-				{
-					subCategory: 'Careers',
-					subCategoryLink: '/blog'
-				}
-			]
-		},
-		{
-			category: 'Get in touch',
-			subCategories: [
-				{
-					subCategory: 'Contact',
-					subCategoryLink: '/contact'
-				},
-				{
-					subCategory: 'Support',
-					subCategoryLink: '/contact'
-				},
-				{
-					subCategory: 'Join us',
-					subCategoryLink: '/contact'
-				}
-			]
-		}
-	],
-	subFooter: {
-		copywriteText: '© Foxi 2024.'
-	}
+  footerAbout: {
+    title: 'Foxi.',
+    aboutText:
+      'Expertly made, responsive, accessible components in React and HTML ready to be used on your website or app. Just copy and paste them on your Tailwind CSS project.',
+    logo: {
+      src: '/logo.svg',
+      alt: 'The tailwind astro theme',
+      text: 'Foxi.'
+    }
+  },
+
+  footerColumns: [
+    // {
+    //   category: 'Product',
+    //   subCategories: [
+    //     { subCategory: 'Features',   subCategoryLink: '/features' },
+    //     { subCategory: 'FAQ',        subCategoryLink: '/faq' },
+    //     { subCategory: 'Pricing',    subCategoryLink: '/pricing' },
+    //     { subCategory: 'Changelog',  subCategoryLink: '/changelog' },
+    //     { subCategory: 'Terms',      subCategoryLink: '/terms' }
+    //   ]
+    // },
+
+    // {
+    //   category: 'About us',
+    //   subCategories: [
+    //     { subCategory: 'About us', subCategoryLink: '/' },
+    //     { subCategory: 'News',     subCategoryLink: '/blog' },
+    //     { subCategory: 'Careers',  subCategoryLink: '/blog' }
+    //   ]
+    // },
+
+    {
+      category: 'Get in touch',
+      subCategories: [
+        { subCategory: 'Contact', subCategoryLink: '/contact' },
+
+        // { subCategory: 'Support', subCategoryLink: '/contact' },
+        // { subCategory: 'Join us', subCategoryLink: '/contact' }
+      ]
+    }
+  ],
+
+  subFooter: {
+    copywriteText: '© Foxi 2024.'
+  }
 }
